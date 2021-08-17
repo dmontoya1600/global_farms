aff53a21ac34b0e37b9c49353564640b8a4b1e44
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-08-17 01:51:35 -0500

    fixed news issue. should work on heroku now. now going to work on styling for farm page/news and deploying to heroku

---
+++ react-app/public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "REACT_APP_SUB_KEY": "d53476576b444974a8c6a3eff7fbc1f4"
+};
--- react-app/src/components/HomePage/News.js
+++ react-app/src/components/HomePage/News.js
@@ -1,30 +1,44 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams, Route } from 'react-router-dom';
+import env from "react-dotenv";
 
-function News() {
+
+function News({newsKey}) {
+
     let [articles, setArticles] = useState(null)
     let [newsContent, setNewsContent] = useState(false)
     let [newsUrl, setNewsUrl] = useState(null)
-    useEffect(() => {
-        (async () => {
-            let res = await fetch('https://newsapi.org/v2/top-headlines?q=farms&apiKey=da70b2caa6fa47ce88c6ea7632d2f8a4')
 
-            let newsObj = await res.json()
-            if(newsObj.status === 'ok'){
-                setArticles(newsObj.articles)
-            }
+    useEffect(() => {
+        if(!newsKey){
+            newsKey = env?.REACT_APP_SUB_KEY
+            if(!newsKey) newsKey = process.env.REACT_APP_SUB_KEY
+        }
+        (async () => {
+            let res = await fetch('https://api.bing.microsoft.com/v7.0/news/search?mkt=en-us&q=farms', {
+                method: 'GET',
+                headers: {
+                    "Ocp-Apim-Subscription-Key" : newsKey
+                }
+            })
+            let data = await res.json()
+
+            setArticles(data.value)
+
         })();
     }, [])
+
+
   return (
     <div className='news__component'>
-        {newsUrl ? <Route exact path="/" render={() => (window.location = newsUrl)} />: null}
+        {newsUrl ? <Route hidden exact path="/" render={() => (window.location = newsUrl)} />: null}
         {articles ? articles.map(article => (
-            <div className='news__article' key={article.title} onClick={() => setNewsUrl(article.url)} onMouseEnter={() => setNewsContent(article.title)} onMouseLeave={() => setNewsContent(false)}>
-                <div className='news__source'>{article.source.name}</div>
-                <img className='news__image' src={article.urlToImage}/>
-                <div className='news__title' >{article.title}</div>
-                {newsContent === article.title ?
+            <div className='news__article' key={article.name} onClick={() => setNewsUrl(article.url)} onMouseEnter={() => setNewsContent(article.name)} onMouseLeave={() => setNewsContent(false)}>
+                <div className='news__source'>{article.provider[0].name}</div>
+                <img className='news__image' src={article.image?.thumbnail?.contentUrl}/>
+                <div className='news__title' >{article.name}</div>
+                {newsContent === article.name ?
                 <div className='news__desc'>{article.description}</div>: null}
             </div>
         )) : <div>No recent farming news</div>}
